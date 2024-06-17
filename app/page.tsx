61acb038ed7bb592345c8cdcb69d592d929ef504
Rich Leach <rich@cfsnap.com>
2024-06-17 14:22:35 -0600

    remedied missing component

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,6 @@ import {
 import { Navbar, NavbarDivider, NavbarItem, NavbarLabel, NavbarSection, NavbarSpacer } from './components/navbar'
 import { Sidebar, SidebarBody, SidebarHeader, SidebarItem, SidebarLabel, SidebarSection } from './components/sidebar'
 import { StackedLayout } from './components/stacked-layout'
-import { Sample } from './components/pages/Sample'
 import {
   ArrowRightStartOnRectangleIcon,
   ChevronDownIcon,
@@ -57,90 +56,8 @@ function TeamDropdownMenu() {
 
 export default function Example() {
   return (
-      <StackedLayout
-          navbar={
-            <Navbar>
-              <Dropdown>
-                <DropdownButton as={NavbarItem} className="max-lg:hidden">
-                  <Avatar src="/tailwind-logo.svg" />
-                  <NavbarLabel>Tailwind Labs</NavbarLabel>
-                  <ChevronDownIcon />
-                </DropdownButton>
-                <TeamDropdownMenu />
-              </Dropdown>
-              <NavbarDivider className="max-lg:hidden" />
-              <NavbarSection className="max-lg:hidden">
-                {navItems.map(({ label, url }) => (
-                    <NavbarItem key={label} href={url}>
-                      {label}
-                    </NavbarItem>
-                ))}
-              </NavbarSection>
-              <NavbarSpacer />
-              <NavbarSection>
-                <NavbarItem href="/search" aria-label="Search">
-                  <MagnifyingGlassIcon />
-                </NavbarItem>
-                <NavbarItem href="/inbox" aria-label="Inbox">
-                  <InboxIcon />
-                </NavbarItem>
-                <Dropdown>
-                  <DropdownButton as={NavbarItem}>
-                    <Avatar src="/profile-photo.jpg" square />
-                  </DropdownButton>
-                  <DropdownMenu className="min-w-64" anchor="bottom end">
-                    <DropdownItem href="/my-profile">
-                      <UserIcon />
-                      <DropdownLabel>My profile</DropdownLabel>
-                    </DropdownItem>
-                    <DropdownItem href="/settings">
-                      <Cog8ToothIcon />
-                      <DropdownLabel>Settings</DropdownLabel>
-                    </DropdownItem>
-                    <DropdownDivider />
-                    <DropdownItem href="/privacy-policy">
-                      <ShieldCheckIcon />
-                      <DropdownLabel>Privacy policy</DropdownLabel>
-                    </DropdownItem>
-                    <DropdownItem href="/share-feedback">
-                      <LightBulbIcon />
-                      <DropdownLabel>Share feedback</DropdownLabel>
-                    </DropdownItem>
-                    <DropdownDivider />
-                    <DropdownItem href="/logout">
-                      <ArrowRightStartOnRectangleIcon />
-                      <DropdownLabel>Sign out</DropdownLabel>
-                    </DropdownItem>
-                  </DropdownMenu>
-                </Dropdown>
-              </NavbarSection>
-            </Navbar>
-          }
-          sidebar={
-            <Sidebar>
-              <SidebarHeader>
-                <Dropdown>
-                  <DropdownButton as={SidebarItem} className="lg:mb-2.5">
-                    <Avatar src="/tailwind-logo.svg" />
-                    <SidebarLabel>Tailwind Labs</SidebarLabel>
-                    <ChevronDownIcon />
-                  </DropdownButton>
-                  <TeamDropdownMenu />
-                </Dropdown>
-              </SidebarHeader>
-              <SidebarBody>
-                <SidebarSection>
-                  {navItems.map(({ label, url }) => (
-                      <SidebarItem key={label} href={url}>
-                        {label}
-                      </SidebarItem>
-                  ))}
-                </SidebarSection>
-              </SidebarBody>
-            </Sidebar>
-          }
-      >
-        {children}
-      </StackedLayout>
+      <div>
+        Home page.
+      </div>
   )
 }
